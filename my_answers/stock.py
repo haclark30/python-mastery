cc3b6a1b5e5130b501310703d4ea57f63e42e4a1
Harrison Clark <haclark30@gmail.com>
2023-10-25 07:06:06 -0500

    exercise 3_4

--- my_answers/stock.py
+++ my_answers/stock.py
@@ -3,27 +3,53 @@ from decimal import Decimal
 
 
 class Stock:
-    types = (str, int, float)
+    __slots__ = ('name', '_shares', '_price')
+    _types = (str, int, float)
 
     def __init__(self, name: str, shares: int, price: float):
         self.name = name
-        self.shares = shares
-        self.price = price
+        self._shares = shares
+        self._price = price
 
     @classmethod
     def from_row(cls, row):
-        values = [func(val) for func, val in zip(cls.types, row)]
+        values = [func(val) for func, val in zip(cls._types, row)]
         return cls(*values)
 
+    @property
+    def shares(self) -> int:
+        return self._shares
+
+    @shares.setter
+    def shares(self, value):
+        if not isinstance(value, self._types[1]):
+            raise TypeError(f'Expected {self._types[1].__name__}')
+        if value < 0:
+            raise AttributeError('Shares must be non-negative')
+        self._shares = value
+
+    @property
+    def price(self) -> float:
+        return self._price
+
+    @price.setter
+    def price(self, value):
+        if not isinstance(value, self._types[2]):
+            raise TypeError(f'Expected {self._types[2].__name__}')
+        if value < 0:
+            raise AttributeError('Price must be non-negative')
+        self._price = value
+
+    @property
     def cost(self) -> float:
-        return self.shares * self.price
+        return self._shares * self._price
 
     def sell(self, amount):
-        self.shares -= amount
+        self._shares -= amount
 
 
 class DStock(Stock):
-    types = (str, int, Decimal)
+    _types = (str, int, Decimal)
 
 
 def read_portfolio(filename: str) -> "list[Stock]":
@@ -42,7 +68,7 @@ def print_portfolio(portfolio: "list[Stock]"):
     print('%10s %10s %10s' % ("name", "shares", "price"))
     print(('-'*10 + ' ')*3)
     for s in portfolio:
-        print('%10s %10d %10.2f' % (s.name, s.shares, s.price))
+        print('%10s %10d %10.2f' % (s.name, s._shares, s._price))
 
 
 if __name__ == "__main__":
